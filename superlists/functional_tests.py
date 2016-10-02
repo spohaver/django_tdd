f98ada289103d208714adff0c04e4c50675d15d8
Sean OHaver <spohaver@gmail.com>
2016-10-01 20:48:37 -0700

    test added for first new item

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -41,10 +41,7 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(5)
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy some games' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        self.assertIn('1: Buy some games', [row.text for row in rows])
 
         # There is still a text box inviting her to add another item. He enters
         # "Buy some downloadable content"
