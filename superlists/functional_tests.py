1d01bd91a5764fec965283c0186e942909ceedbe
Sean OHaver <spohaver@gmail.com>
2016-10-03 14:24:48 -0700

    refactored test with helper method check_for_row_in_list_table

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_start_list_and_retrieve_it_later(self):
         # Sean has head about a cool new online to-do app.
         # He goes to check its homepage
@@ -36,12 +41,7 @@ class NewVisitorTest(unittest.TestCase):
         # When he hits enter, the page updates, and now the page lists:
         # "1: Buy some games" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-
-        import time
-        time.sleep(5)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy some games', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy some games')
 
         # There is still a text box inviting her to add another item. He enters
         # "Buy some downloadable content"
@@ -50,12 +50,8 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # The page updates again, and now shows both items in the list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy some games', [row.text for row in rows])
-        self.assertIn('2: Buy some downloadable content',
-                      [row.text for row in rows]
-                      )
+        self.check_for_row_in_list_table('1: Buy some games')
+        self.check_for_row_in_list_table('2: Buy some downloadable content')
 
         # He wonders whether the site will remember her list. Then he sees that
         # the site has generated a unique URL for him -- there is some
