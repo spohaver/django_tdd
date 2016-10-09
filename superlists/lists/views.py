58867f63dea4318cac09449ed24a4c872c5f94cb
Sean OHaver <spohaver@gmail.com>
2016-10-08 19:15:27 -0700

    adding test for all items, and show all items in template

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -6,16 +6,6 @@ def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
         return redirect('/')
-#        new_item_text = request.POST['item_text']
-#        Item.objects.create(text=new_item_text)
-#    else:
-#        new_item_text = ''
 
-#    item = Item()
-#    item.text = request.POST.get('item_text', '')
-#    item.save()
-
-#    return render(request, 'home.html', {
-#        'new_item_text': new_item_text,
-#    })
-    return render(request, 'home.html')
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
