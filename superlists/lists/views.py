728882d1b6568ee80b60b958abca36c3e87f2191
Sean OHaver <spohaver@gmail.com>
2016-10-08 18:47:24 -0700

    Adding in the "Always redirect after a POST", commenting out old code instead of removing it. will be removed in later commit

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import redirect, render
+from lists.models import Item
 
 # Create your views here.
 def home_page(request):
-#    if request.method == 'POST':
-#        return HttpResponse(request.POST['item_text'])
-    return render(request, 'home.html', {
-        'new_item_text': request.POST.get('item_text', ''),
-    })
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+#        new_item_text = request.POST['item_text']
+#        Item.objects.create(text=new_item_text)
+#    else:
+#        new_item_text = ''
+
+#    item = Item()
+#    item.text = request.POST.get('item_text', '')
+#    item.save()
+
+#    return render(request, 'home.html', {
+#        'new_item_text': new_item_text,
+#    })
+    return render(request, 'home.html')
